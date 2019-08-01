fca97f338e19baad16e7443a1b8212b0675949e8
Kyle Taylor <kyle.a.taylor@gmail.com>
2019-07-31 22:10:46 -0600

    putting it up for the night

--- woody/random_forest_predict_across_geotiff_fragments.py
+++ woody/random_forest_predict_across_geotiff_fragments.py
@@ -35,6 +35,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Use a Random Forests classifier to predict across Earth Engine GeoTIFF fragments')
 parser.add_argument('--dir', metavar="d", dest='geotiff_dir_path', default='.',help='specify the full path to a directory containing EE GeoTIFF files')
 parser.add_argument('--target_dir', metavar="t", dest='splits_target_dir', default='splits',help='specify the local path to a target directory to house our split EE GeoTIFF files')
+parser.add_argument('--outfile', metavar="o", dest='outfile', default='rf_prediction.tif', help="specify the filename for our merged random forests prediction raster")
 
 args = parser.parse_args()
 
@@ -42,13 +43,14 @@ args = parser.parse_args()
 # Local functions
 #
 
-def dig_path_for_tifs(**kwargs):
+def dig_path(**kwargs):
   PATH = kwargs.get('path', '.')
-  return glob.glob(os.path.join(PATH, '/*.tif'), recursive=False)
+  EXTENSION_PATTERN = kwargs.get('extension_pattern', '*.tif')
+  return glob.glob(os.path.join(PATH, EXTENSION_PATTERN), recursive=False)
 
 def gdal_retile(**kwargs):
   """
-  Wrapper for gdal_retile.py -- e.g., : ls -1 *.tif | awk '{ print "gdal_retile.py -ps 2500 2500 -targetDir splits/ " $1 }' | /bin/bash
+  Wrapper for gdal_retile.py
   """
   GEOTIFF_FILES = kwargs.get('geotiff_files', None)
   TARGET_DIR = kwargs.get('target_dir', args.splits_target_dir)
@@ -60,7 +62,9 @@ def gdal_retile(**kwargs):
     raise ValueError("geotiff_files= argument cannot be empty")
 
   if os.path.exists(TARGET_DIR):
-    logger.debug("Existing gdal_retile target directory found : " + TARGET_DIR + "; Assuming you want to use existing tiles and skipping GeoTIFF file splitting operations")
+    logger.debug("Existing gdal_retile target directory found : " + \
+      TARGET_DIR + \
+      "; Assuming you want to use existing tiles and skipping GeoTIFF file splitting operations")
     return
   else:
     os.mkdir(TARGET_DIR)
@@ -69,17 +73,26 @@ def gdal_retile(**kwargs):
   for segment in GEOTIFF_FILES:
     subprocess.run(["gdal_retile.py", "-ps", str(PIXEL_SIZE), str(PIXEL_SIZE), "-targetDir", TARGET_DIR, segment])
     progress += 1
-    logger.debug("Percent complete: "+str(round(progress/len(GEOTIFF_FILES), 2)))
+    logger.debug("Overall % complete: "+str(round(progress/len(GEOTIFF_FILES), 2)))
 
 def rf_predict(**kwargs):
   """
-  Wrapper for Rscript -- e.g., Rscript random_forest_predict_across_geotiff_fragments.R $PWD;
+  Wrapper for Rscript
   """
-  R_SCRIPT_SOURCE_PATH = kwargs.get('r_script_path', './random_forest_predict_across_geotiff_fragments.R')
-  GEOTIFF_SEGMENTS_DIR = kwargs.get('geotiff_segments_path', './' + args.splits_target_dir)
-  subprocess.run("RScript", R_SCRIPT_SOURCE_PATH, GEOTIFF_SEGMENTS_DIR)
+  R_SCRIPT_SOURCE_PATH = kwargs.get('r_script_path',
+    os.path.join('.','random_forest_predict_across_geotiff_fragments.R'))
+  GEOTIFF_SEGMENTS_DIR = kwargs.get('geotiff_segments_path',
+    os.path.join('.',args.splits_target_dir))
+  subprocess.run("RScript", R_SCRIPT_SOURCE_PATH,
+    GEOTIFF_SEGMENTS_DIR)
 
-
+def gdal_merge(**kwargs):
+  """
+  Wrapper for gdal_merge : e.g., gdal_merge.py -o "../$DESTINATION_FILENAME" *_prediction.tif
+  """
+  GEOTIFF_SEGMENTS_DIR = kwargs.get('geotiff_segments_path', os.path.join('.', args.splits_target_dir))
+  logger.debug("Calling gdal_mergy.py using our prediction segments")
+  subprocess.run("gdal_merge.py",'-o', args.outfile, os.path.join(GEOTIFF_SEGMENTS_DIR,'*_prediction.tif'))
 #
 # Main
 #
@@ -90,19 +103,26 @@ if __name__ == "__main__":
 
   logger.debug("Parsing input directory for files: " + os.path.join(args.geotiff_dir_path,'*.tif'))
   
-  geotiff_files = dig_path_for_tifs(path=args.geotiff_dir_path)
+  geotiff_files = dig_path(path=args.geotiff_dir_path)
   logger.debug("Found "+str(len(geotiff_files))+" raster files")
   logger.debug("Wrapping gdal_retile.py using Earth Engine segments")
 
   try:
-    gdal_retile(geotiff_files=geotiff_files, target_dir=os.path.join(args.geotiff_dir_path, args.splits_target_dir))
+    gdal_retile(geotiff_files=geotiff_files,
+      target_dir=os.path.join(args.geotiff_dir_path, args.splits_target_dir))
   except ValueError as e:
-    logger.debug("Cannot pass gdal_retile.py an empty list of GeoTIFFs -- Exiting")
+    logger.debug("Cannot pass gdal_retile.py an empty list of GeoTIFFs -- this shouldn't happen; quitting")
     sys.exit(1)
-  if len(dig_path_for_tifs(path=os.path.join(args.geotiff_dir_path, args.splits_target_dir))) == 0:
-    logger.debug("gdal_retile target 'splits' directory didn't appear to contain any GeoTIFF files -- quitting")
+  if len(dig_path(path=os.path.join(args.geotiff_dir_path, args.splits_target_dir))) == 0:
+    logger.debug("gdal_retile target 'splits' directory didn't appear to contain any GeoTIFF files -- this shouldn't happen; quitting")
     sys.exit(1)
 
   logger.debug("Launching 'RScript' interface with our fitted Random Forests model object")
   
   rf_predict(geotiff_segments_path=os.path.join(args.geotiff_dir_path, args.splits_target_dir))
+
+  if len(dig_path(path=os.path.join(args.geotiff_dir_path, args.splits_target_dir), extension_pattern='*_prediction.tif')) < 1 :
+    logger.debug("Couldn't find any *_prediction.tif products in the 'splits' directory -- this shouldn't happen; quitting")
+    sys.exit(1)
+  else:
+    gdal_merge(geotiff_segments_path=os.path.join(args.geotiff_dir_path, args.splits_target_dir))
